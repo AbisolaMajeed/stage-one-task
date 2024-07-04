319214c885073e47066d23ea91afe0207b519ed8
AbisolaMajeed <abisola.majeed@gmail.com>
2024-07-04 19:08:27 +0100

    update

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,8 @@ use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function(){
-    return "Welcome to ".config('app.name')." (Application Programming Interface)";
-});
+// Route::get('/', function(){
+//     return "Welcome to ".config('app.name')." (Application Programming Interface)";
+// });
 
-Route::get('/hello', [HomeController::class, 'getDetails']);
+Route::get('/', [HomeController::class, 'getDetails']);
